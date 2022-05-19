65af37483e5ebfb3110d0d1ed96757f4b9e32aac
imaducklol <orionhess@gmail.com>
2022-05-19 00:14:06 -0700

    More class work and initial input system

--- main.cpp
+++ main.cpp
@@ -1,16 +1,90 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-class lilDude {
+class Particle {
 public:
+    int id;
     int state;
-    int mass;
-    int velocity;
+    double mass;
+    double velocity;
+    double direction;
+    Particle(int s = 0, double m = 10, double v = 0, double d = 0) {
+        state = s;
+        mass = m;
+        velocity = v;
+        direction = d;
+    }
+
 };
 
-int main()
-{
+void ListParticles(const vector<Particle>& particles) {
+    if (particles.size() == 0) cout << "None yet" << endl;
+    else
+    for (Particle particle : particles) {
+        cout <<
+        particle.id    << endl <<
+        particle.state << endl <<
+        particle.mass  << endl << endl;
+    }
+}
+
+void AddParticle(vector<Particle> *particles) {
+    Particle particle;
+    double input;
+
+    particle.id = particles->size();
+
+    cout << "dynamic(0), static(1), or mouse controlled(2)\n";
+    cin >> input;
+    particle.state = input;
+
+    cout << "mass\n";
+    cin >> input;
+    particle.mass = input;
+
+    cout << "velocity\n";
+    cin >> input;
+    particle.velocity = input;
+
+    cout << "dynamic(0), static(1), or mouse controlled(2)\n";
+    cin >> input;
+    particle.direction = input;
+
+    particles->push_back(particle);
+}
+
+int main() {
+    // Particle setup
+    double gConst = 6.6743 * pow(10, 2);
+    vector<Particle> particles;
+
+    while (true) {
+        int input;
+        cout << "Add Particle (1), list particles (2), run simulation (3)\n";
+        cin >> input;
+
+        switch (input) {
+            case 1: {
+
+                break;
+            }
+            case 2: {
+                ListParticles(particles);
+                break;
+            }
+            case 3: {
+                goto programStart;
+            }
+            default: {
+                cout << "That wasn't a listed option now was it\n";
+                break;
+            }
+        }
+    }
+    programStart:
+
     sf::RenderWindow window(sf::VideoMode(200, 200), "Simulation");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
